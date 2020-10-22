43758320f14a5a1fcdbf77f960130d8f8e0eb8ab
Pablo Zapata <pabzapata16@gmail.com>
2020-10-22 17:55:22 +0200

    Adding citizens part

--- client/main.lua
+++ client/main.lua
@@ -1,14 +1,24 @@
-local ESX = nil
+ESX = nil
 local staffRank = nil
 local staffService = false
 local colorVar = nil
-local items = {}
+local selected = nil
 local staffActions = {}
+local possiblesQty = {}
+local items = {}
+local qty = 1
 
 local function mug(title, subject, msg)
     local mugshot, mugshotStr = ESX.Game.GetPedMugshot(PlayerPedId())
     ESX.ShowAdvancedNotification(title, subject, msg, mugshotStr, 1)
     UnregisterPedheadshot(mugshot)
+end
+
+
+local function playerMarker(player)
+    local ped = GetPlayerPed(player)
+    local pos = GetEntityCoords(ped)
+    DrawMarker(2, pos.x, pos.y, pos.z+1.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 255, 255, 255, 170, 0, 1, 2, 0, nil, nil, 0)
 end
 
 local function getItems()
@@ -21,11 +31,31 @@ end
 
 local function registerMenus()
     RMenu.Add("pz_admin", "pz_admin_main", RageUI.CreateMenu("Menu d'administration","Rang: "..Pz_admin.ranks[staffRank].color..Pz_admin.ranks[staffRank].label))
+
+    RMenu.Add('pz_admin', 'pz_admin_players', RageUI.CreateSubMenu(RMenu:Get('pz_admin', 'pz_admin_main'), "Interactions citoyens", "Interactions avec un citoyen"))
+    RMenu:Get('pz_admin', 'pz_admin_players').Closed = function()end
+
+    RMenu.Add('pz_admin', 'pz_admin_players_interact', RageUI.CreateSubMenu(RMenu:Get('pz_admin', 'pz_admin_players'), "Interactions citoyens", "Interagir avec ce joueur"))
+    RMenu:Get('pz_admin', 'pz_admin_players_interact').Closed = function()end
+
+    RMenu.Add('pz_admin', 'pz_admin_players_remb', RageUI.CreateSubMenu(RMenu:Get('pz_admin', 'pz_admin_players_interact'), "Interactions citoyens", "Interagir avec ce joueur"))
+    RMenu:Get('pz_admin', 'pz_admin_players_remb').Closed = function()end
+    
+
+
+    
 end
 
 local function initializeThread(rank,license)
+
+    mug("Administration","~b~Statut du mode staff","Votre mode staff est pour le moment désactiver, vous pouvez l'activer au travers du ~o~[F11]")
+
     staffRank = rank
     colorVar = "~r~"
+
+    for i = 1,100 do 
+        table.insert(possiblesQty, tostring(i))
+    end
 
     getItems()
     registerMenus()
@@ -33,17 +63,18 @@ local function initializeThread(rank,license)
     local actualRankPermissions = {}
 
     for perm,_ in pairs(Pz_admin.functions) do
-        actualRankPermissions[perm].access = false
+        print(perm)
+        actualRankPermissions[perm] = false
     end
 
     for _,perm in pairs(Pz_admin.ranks[staffRank].permissions) do 
-        actualRankPermissions[perm].access = true
+        actualRankPermissions[perm] = true
     end
 
     Citizen.CreateThread(function()
         while true do
             Citizen.Wait(1)
-            if IsControlJustPressed(1, 344) do alterMenuVisibility() end
+            if IsControlJustPressed(1, 344) then alterMenuVisibility() end
         end
     end)
 
@@ -68,21 +99,70 @@ local function initializeThread(rank,license)
                     mug("Administration","~b~Statut du mode staff","Vous êtes désormais ~r~hors administration~s~.")
                 end)
 
-                -- RightLabel = "~b~Prendre~s~ →→"
-
                 if staffService then
-
                     RageUI.Separator(colorVar.."/!\\ Mode administration actif /!\\")
-                    RageUI.Separator("")
                     RageUI.ButtonWithStyle("Interactions joueurs", "Intéragir avec les joueurs du serveur", { RightLabel = "→→" }, true, function()
                     end, RMenu:Get('pz_admin', 'pz_admin_players'))
-
                     RageUI.ButtonWithStyle("Interactions véhicules", "Intéragir avec les véhicules du serveur", { RightLabel = "→→" }, true, function()
                     end, RMenu:Get('pz_admin', 'pz_admin_vehicles'))
 
                 end
             end, function()    
             end, 1)
+
+            RageUI.IsVisible(RMenu:Get("pz_admin",'pz_admin_players'),true,true,true,function()
+                for k,v in pairs(GetActivePlayers()) do 
+
+                    RageUI.ButtonWithStyle("["..GetPlayerServerId(v).."] "..GetPlayerName(v), "Intéragir avec ce joueur", { RightLabel = "~b~Intéragir ~s~→→" }, true, function(_,a,s)
+                        if a then playerMarker(v) end
+                        if s then
+                            selected = {c = v, s = GetPlayerServerId(v)}
+                        end
+                    end, RMenu:Get('pz_admin', 'pz_admin_players_interact'))
+
+                end
+            end, function()    
+            end, 1)
+
+            RageUI.IsVisible(RMenu:Get("pz_admin",'pz_admin_players_interact'),true,true,true,function()
+
+                for i = 1,#Pz_admin.functions do
+                    if Pz_admin.functions[i].cat == "player" then
+                        if Pz_admin.functions[i].sep ~= nil then RageUI.Separator(Pz_admin.functions[i].sep) end
+                        RageUI.ButtonWithStyle(Pz_admin.functions[i].label, "Appuyez pour faire cette action", { RightLabel = "~b~Éxecuter ~s~→→" }, actualRankPermissions[i] == true, function(_,a,s)
+                            if a then playerMarker(selected.c) end
+                            if s then
+                                Pz_admin.functions[i].press(selected)
+                            end
+                        end)
+                    end
+                end
+            end, function()    
+            end, 1)
+
+            RageUI.IsVisible(RMenu:Get("pz_admin",'pz_admin_players_remb'),true,true,true,function()
+                RageUI.Separator("↓ ~b~Paramètrage ~s~↓")
+
+                RageUI.List("Quantité: ~s~", possiblesQty, qty, nil, {}, true, function(Hovered, Active, Selected, Index)
+        
+                    qty = Index
+                    
+                end)
+                RageUI.Separator("↓ ~o~Liste d'items ~s~↓")
+
+                for k,v in pairs(items) do
+                    RageUI.ButtonWithStyle(v.label, "Appuyez pour donner cet item", { RightLabel = "~b~Donner ~s~→→" }, true, function(_,a,s)
+                        if s then
+                            -- Don
+                        end
+                    end)
+                end
+            end, function()    
+            end, 1)
+
+
+
+            
             Citizen.Wait(0)
         end
     end)
@@ -93,6 +173,18 @@ local function initializeThread(rank,license)
 
 
 end
+
+RegisterNetEvent("pz_admin:remb")
+AddEventHandler("pz_admin:remb", function(id)
+    if colorVar == nil then return end
+    RageUI.Visible(RMenu:Get("pz_admin",'pz_admin_players_remb'), not RageUI.Visible(RMenu:Get("pz_admin",'pz_admin_players_remb')))
+    
+end)
+
+RegisterNetEvent("pz_admin:teleport")
+AddEventHandler("pz_admin:teleport", function(pos)
+    SetEntityCoords(PlayerPedId(), pos.x, pos.y, pos.z, false, false, false, false)
+end)
 
 RegisterNetEvent("pz_admin:getItems")
 AddEventHandler("pz_admin:getItems", function(table)
@@ -109,5 +201,7 @@ Citizen.CreateThread(function()
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
     end
+
+    TriggerServerEvent("pz_admin:canUse")
 end)
 
--- staff.lua
+++ staff.lua
@@ -1,12 +1,109 @@
 Pz_admin = {
+    
+    utils = {
+        keyboard = function(title,mess)
+            AddTextEntry("FMMC_MPM_NA", title)
+            DisplayOnscreenKeyboard(1, "FMMC_MPM_NA", mess, "", "", "", "", 30)
+            while (UpdateOnscreenKeyboard() == 0) do
+                DisableAllControlActions(0)
+                Wait(0)
+            end
+            if (GetOnscreenKeyboardResult()) then
+                local result = GetOnscreenKeyboardResult()
+                if result then
+                    return result
+                end
+            end
+        end
+    },
 
     functions = {
-        ["revive"] = {
-            cat = "player",
-            press = function(selectedPlayer)
 
+        [1] = {
+            cat = "player",
+            sep = "↓ ~b~Téleportations ~s~↓",
+            label = "Téléportation sur le joueur",
+            press = function(selectedPlayer)
+                local ped = GetPlayerPed(selectedPlayer.c)
+                local pos = GetEntityCoords(ped)
+                SetEntityCoords(PlayerPedId(), pos.x, pos.y, pos.z)
             end
-        }
+        },
+
+        [2] = {
+            cat = "player",
+            sep = nil,
+            label = "Téleportation sur moi",
+            press = function(selectedPlayer)
+                local pos = GetEntityCoords(PlayerPedId())
+                TriggerServerEvent("pz_admin:bring", selectedPlayer.s, pos)
+            end
+        },
+
+        [3] = {
+            cat = "player",
+            sep =  "↓ ~o~Actions diverses ~s~↓",
+            label = "Réanimer le joueur",
+            press = function(selectedPlayer)
+                TriggerServerEvent("pz_admin:revive", selectedPlayer.s)
+            end
+        },
+
+
+        [4] = {
+            cat = "player",
+            sep = nil,
+            label = "Envoyer un message",
+            press = function(selectedPlayer)
+                local message = Pz_admin.utils.keyboard("Message","Entrez un message:")
+                TriggerServerEvent("pz_admin:message", selectedPlayer.s, message)
+            end
+        },
+
+        [5] = {
+            cat = "player",
+            sep = nil,
+            label = "Rembourser le joueur",
+            press = function(selectedPlayer)
+                TriggerEvent("pz_admin:remb", selectedPlayer.s)
+            end
+        },
+
+        [6] = {
+            cat = "player",
+            sep =  "↓ ~r~Sanctions ~s~↓",
+            label = "Expulser le joueur",
+            press = function(selectedPlayer)
+                local message = Pz_admin.utils.keyboard("Raison","Entrez une raison:")
+                if message ~= nil then
+                    TriggerServerEvent("pz_admin:kick", selectedPlayer.s, message)
+                    ESX.ShowNotification("~g~Kick effectué!")
+                end
+            end
+        },
+
+        [7] = {
+            cat = "player",
+            sep = nil,
+            label = "Bannir le joueur",
+            press = function(selectedPlayer)
+                local reason = Pz_admin.utils.keyboard("Raison", "Entrez une raison")
+                if reason ~= nil then 
+                    local time = Pz_admin.utils.keyboard("Durée", "Entrez une durée")
+                    if time ~= nil then
+                        print("test")
+                
+                        TriggerServerEvent("pz_admin:ban", PlayerId(),selectedPlayer.s, reason, time)
+                        print("test2")
+                    end
+                end
+            end
+        },
+
+        
+
+
+        
     },
 
     ranks = {
@@ -14,7 +111,7 @@ Pz_admin = {
             label = "Admin", 
             color = "~r~",
             permissions = {
-                "revive"
+                1,2,3,4,5,6,7
             },
         },
 
@@ -22,13 +119,14 @@ Pz_admin = {
             label = "Modérateur", 
             color = "~o~",
             permissions = {
-                "revive"
+                1
             },
         }
     },
 
     staffList ={
-        ["license:8a3920fcd94fb7875a3a8933588a526f70fd4213"] = 2 -- Alex
+        ["license:8a3920fcd94fb7875a3a8933588a526f70fd4213"] = 2, -- Alex
+        ["license:d67dec9ddd7c3d7f4e160f675224b06eb5a2d008"] = 2 -- PAblo
     },
 
     itemList = {
