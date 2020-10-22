5103586364d0028f17117f4084accd1fcbbd41ae
Pablo Zapata <pabzapata16@gmail.com>
2020-10-22 14:59:47 +0200

    Setting up dependancies and manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,39 @@
+fx_version 'adamant'
+game 'gta5'
+description 'Pablo Z. Admin'
+version '1.0'
+
+--[[
+
+	   PZ_Admin par Pablo Z
+
+	Tous droits réservés © 2020
+
+]]
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	"server/main.lua",
+	"staff.lua"
+}
+
+client_scripts {
+	-- RageUI Version 1
+	"src/client/RMenu.lua",
+    "src/client/menu/RageUI.lua",
+    "src/client/menu/Menu.lua",
+    "src/client/menu/MenuController.lua",
+    "src/client/components/*.lua",
+    "src/client/menu/elements/*.lua",
+    "src/client/menu/items/*.lua",
+    "src/client/menu/panels/*.lua",
+	"src/client/menu/windows/*.lua",
+
+	-- Pz_admin
+	"client/main.lua" 
+
+}
+
+dependencies {
+	'mysql-async'
+}
